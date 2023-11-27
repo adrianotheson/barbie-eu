8c20b98c0d7a196514e607f46c25861a85540022
mesadelaranja <adriano.filho@estudante.ifms.edu.br>
2023-11-27 08:54:01 -0400

    remoção de código não utilizado e estilos

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+//import App from './App.tsx'
+
+import{createBrowserRouter, RouterProvider,} from "react-router-dom";
+import Header from './componentes/header/Header';
+import Main from './componentes/main/Main';
+const router = createBrowserRouter([
+{
+  path:"/",
+  element: <> <Header/><Main/> </>,
+},
+{
+  path:"/tere",
+  element: <div>oi mundo T</div>,
+}
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+   {/*<App />*/}
+   <RouterProvider router={router}/>
   </React.StrictMode>,
 )
